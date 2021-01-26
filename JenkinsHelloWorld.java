1f10450b899f2eb56b8fa2503d07f555b4a1055a
C M Abdullah Khan <cmabdullah21@gmail.com>
2021-01-26 22:37:13 +0600

    hello jenk 2

--- JenkinsHelloWorld.java
+++ JenkinsHelloWorld.java
@@ -1,7 +1,7 @@
 public class JenkinsHelloWorld{
 
   public static void main(String args[]) { 
-    System.out.println("Hello, jenkins world!"); 
+    System.out.println("Hello jenkins, jenkins world!"); 
   }
 
 }
