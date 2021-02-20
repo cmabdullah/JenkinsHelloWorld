ffa57491bbcd541c2d3749c86b254d193b97fed2
C M Abdullah Khan <cmabdullah21@gmail.com>
2021-02-20 13:53:51 +0600

    20/2/21

--- JenkinsHelloWorld.java
+++ JenkinsHelloWorld.java
@@ -1,7 +1,7 @@
 public class JenkinsHelloWorld{
 
   public static void main(String args[]) { 
-    System.out.println("Hello jenkins 20/2/2021, jenkins world!"); 
+    System.out.println("Hello 2 jenkins 20/2/2021, jenkins world!"); 
   }
 
 }
